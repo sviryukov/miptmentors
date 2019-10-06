2a6504266af0e397bde2cabb21ff627fbf20ff45
sviryukov <lirik1198KZ>
2019-10-06 18:31:20 +0300

    Delete unused files and tags

--- src/components/index/index.js
+++ src/components/index/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import {CssBaseline, Hidden} from "@material-ui/core";
-import Animation from './animation';
 import Main from './main';
 
 export default () => {
     return (
         <React.Fragment>
             <CssBaseline/>
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/109/three.min.js"/>
             <Main/>
         </React.Fragment>
     )
