1b523d1fa07e29060125f4576cb606adc5e9f741
sviryukov <lirik1198KZ>
2019-10-10 13:46:03 +0300

    Optimize main block animation

--- src/components/index/main/animation/circle-rolling.js
+++ src/components/index/main/animation/circle-rolling.js
@@ -1,18 +1,16 @@
 import mojs from '@mojs/core';
 
-const build = () => {
-    return new mojs.Shape({
-        parent: document.getElementById('container'),
-        fill: 'none',
-        radius: 190,
-        stroke: 'cyan',
-        strokeWidth: 6,
-        strokeDasharray: '100%',
-        strokeDashoffset: '90%',
-        angle: {'0': '1800'},
-        duration: 27000,
-        easing: 'linear.none'
-    });
-};
+const build = () => new mojs.Shape({
+    parent: document.getElementById('container'),
+    fill: 'none',
+    radius: 190,
+    stroke: 'cyan',
+    strokeWidth: 6,
+    strokeDasharray: '100%',
+    strokeDashoffset: '90%',
+    angle: {'0': '1800'},
+    duration: 27000,
+    easing: 'linear.none'
+});
 
 export { build };
