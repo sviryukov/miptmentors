2ebb1c716a893eeba1708f4ba2f633bcc027f9b8
sviryukov <lirik1198KZ>
2019-10-13 19:11:02 +0300

    Shorten code, rename timeline

--- src/components/index/main/animation/index.js
+++ src/components/index/main/animation/index.js
@@ -1,10 +1,6 @@
 import React, {useEffect} from 'react';
 import {Grid, Box} from "@material-ui/core";
 import {makeStyles} from "@material-ui/styles";
-let mojs,
-    circle75, circle90, circle15_25, circle15_55, circle100,
-    burst,
-    circleStatic, circleRolling;
 
 const useStyles = makeStyles({
     container: {
@@ -23,24 +19,23 @@ export default () => {
     const classes = useStyles();
     useEffect(() => {
 
-        mojs = require('@mojs/core');
-
-        circle90 = require('./circle90');
-        circle15_25 = require('./circle15_25');
-        circle15_55 = require('./circle15_55');
-        circle75 = require('./circle75');
-        circle100 = require('./circle100');
-        burst = require('./burst');
-        circleStatic = require('./circle-static');
-        circleRolling = require('./circle-rolling');
+        const mojs = require('@mojs/core');
+        const circle90 = require('./circle90');
+        const circle15_25 = require('./circle15_25');
+        const circle15_55 = require('./circle15_55');
+        const circle75 = require('./circle75');
+        const circle100 = require('./circle100');
+        const burst = require('./burst');
+        const circleStatic = require('./circle-static');
+        const circleRolling = require('./circle-rolling');
 
         const shapes = [circle90.build(), circle15_25.build(), circle15_55.build(), circle75.build(), circle100.build(),
             burst.build(12000), burst.build(21000), burst.build(26000), circleStatic.build()];
 
-        const timeline = new mojs.Timeline({ repeat: 3600 }).add(shapes, circleRolling.build(false));
+        const mainTimeline = new mojs.Timeline({ repeat: 3600 }).add(shapes, circleRolling.build(false));
         const initialTimeline = new mojs.Timeline({
             onStart: () => document.getElementsByClassName(classes.animation)[0].style.backgroundImage = 'none',
-            onComplete: () => timeline.play()
+            onComplete: () => mainTimeline.play()
         }).add(shapes, circleRolling.build(true)).play();
 
     }, []);
