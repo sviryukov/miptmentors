829fb11883579e67660f04d73cde1cb073eba57c
sviryukov <lirik1198KZ>
2019-10-15 01:38:20 +0300

    Ad mount animation to main page rest of page component

--- src/components/index/rest-page/index.js
+++ src/components/index/rest-page/index.js
@@ -1,29 +1,31 @@
-import React, {useEffect, useState} from 'react'
+import React, {useEffect, useRef, useState} from 'react'
 import {Grid} from "@material-ui/core";
 import {makeStyles} from "@material-ui/styles";
 
 const useStyles = makeStyles({
-    container: {
+    restPage: {
         position: 'relative',
-        top: '-100px',
+        top: '0px',
         minHeight: '2000px',
         margin: 'auto',
         backgroundColor: '#fafafa',
-        transition: 'width 0.1s ease-out'
+        transition: 'width 0.1s, top 0.5s'
     }
 });
 
 export default () => {
     const classes = useStyles();
     const [width, setWidth] = useState(90);
+    const restPage = useRef(null);
     useEffect(() => {
+        restPage.current.style.top = '-100px';
         window.addEventListener('scroll', () => {
             let add_width = window.scrollY < 500 ? 10 * window.scrollY / 500 : 10;
             setWidth(90 + add_width)
         });
     }, []);
     return (
-        <Grid container className={classes.container} style={{ width: width + '%' }}>
+        <Grid container className={classes.restPage} style={{ width: width + '%' }} ref={restPage}>
 
         </Grid>
     );
