9da9a7476cff39a8b4e85d6d1e34478495729ed1
sviryukov <lirik1198KZ>
2019-10-14 13:06:33 +0300

    Change navbar sign-in link hover animation

--- src/components/common/navbar/menu/signin.js
+++ src/components/common/navbar/menu/signin.js
@@ -1,30 +1,54 @@
 import React from 'react';
-import {ListItem, IconButton} from '@material-ui/core';
+import {Link, ListItem} from '@material-ui/core';
 import {makeStyles} from '@material-ui/styles';
 import {PersonOutlineOutlined} from "@material-ui/icons";
 
 const useStyles = makeStyles(theme => ({
     listItem: {
+        position: 'relative',
         width: '80px',
-        height: '102px'
-    },
-    iconButton: {
+        height: '102px',
+        zIndex: 1,
         color: theme.palette.text.light,
         transition: 'all 0.4s',
+        '&::after': {
+            display: 'block',
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            bottom: 0,
+            right: 0,
+            margin: 'auto',
+            width: '100%',
+            height: '1px',
+            content: "'.'",
+            color: 'transparent',
+            background: theme.palette.secondary.main,
+            visibility: 'none',
+            opacity: 0,
+            zIndex: '-1',
+            transition: 'all 0.4s'
+        },
         '&:hover': {
-            backgroundColor: theme.palette.secondary.main,
-            color: theme.palette.primary.main
+            textDecoration: 'none',
+            color: theme.palette.primary.main,
+            '&::after': {
+                opacity: 1,
+                visibility: 'visible',
+                height: '100%'
+            }
         }
+    },
+    icon: {
+        margin: 'auto'
     }
 }));
 
 export default () => {
     const classes = useStyles();
     return (
-        <ListItem className={classes.listItem}>
-            <IconButton href='/signin' className={classes.iconButton}>
-                <PersonOutlineOutlined size="medium"/>
-            </IconButton>
+        <ListItem className={classes.listItem} component={Link} href='/signin'>
+            <PersonOutlineOutlined size="medium" className={classes.icon}/>
         </ListItem>
     );
 };
