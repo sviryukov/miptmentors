640ed309c9104365820b914724da7d6d711b56f3
sviryukov <lirik1198KZ>
2019-10-14 12:51:10 +0300

    Fix animation bug

--- src/components/index/index.js
+++ src/components/index/index.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {CssBaseline} from "@material-ui/core";
 import Main from './main';
 import RestPage from './rest-page';
 
 export default () => {
+    useEffect(() => {
+        if ('scrollRestoration' in window.history) {
+            window.history.scrollRestoration = 'manual';
+        }
+        window.scrollTo(0, 0);
+    }, []);
     return (
         <React.Fragment>
             <CssBaseline/>
