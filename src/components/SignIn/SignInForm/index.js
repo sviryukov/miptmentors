ab2d91800efbe1998343e64d3f1f6521001362ff
sviryukov <lirik1198KZ>
2019-10-29 20:22:38 +0300

    Change sign-in form container height

--- src/components/SignIn/SignInForm/index.js
+++ src/components/SignIn/SignInForm/index.js
@@ -7,14 +7,14 @@ import SingInPasswordField from './SingInPasswordField';
 import SingInButton from "./SingInButton";
 
 let useStyles = makeStyles(theme => ({
-    singInContainer: {
+    singInForm: {
         borderRadius: '4px',
         padding: '80px 30px 40px',
         [theme.breakpoints.up('sm')]: {
             padding: '100px 150px 40px'
         },
         [theme.breakpoints.up('md')]: {
-            height: '409px',
+            height: '445px',
             padding: '64px',
             marginTop: '80px',
             backgroundColor: theme.palette.background.paper
@@ -31,7 +31,7 @@ export default () => {
         <Grid component={Box}
               boxShadow={{xs: 0, md: 3}}
               item xs={12} md={5} lg={4} xl={3}
-              className={classes.singInContainer}>
+              className={classes.singInForm}>
             <SingInHeader/>
             <SingInEmailField/>
             <SingInPasswordField/>
