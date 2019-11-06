b8b843f4424b5c7a836081a4ce4ee6bae361449a
sviryukov <lirik1198KZ>
2019-11-06 23:36:07 +0300

    Create pages array to unify pages routing

--- route.js
+++ route.js
@@ -1,47 +1,31 @@
 import express from "express";
 import React from 'react';
-// noinspection ES6CheckImport
-import {renderToString} from 'react-dom/server';
+import ReactDOMServer from 'react-dom/server';
 import {ServerStyleSheets, ThemeProvider} from "@material-ui/styles";
 import template from './src/template';
 import theme from './src/theme';
-import {Home} from './src/components/Home';
-import {News} from './src/components/News';
-import {Mentors} from './src/components/Mentors';
-import {Contacts} from './src/components/Contacts';
-import {FindMentor} from './src/components/FindMentor';
-import {BecomeMentor} from './src/components/BecomeMentor';
-import {SignIn} from './src/components/SignIn';
+import pages from './src/pages';
 
 export default (passport, client) => {
 
     let router = express.Router();
 
-    router.get("/", (request, response) => {
-        const sheets = new ServerStyleSheets();
-        const html = renderToString(
-            sheets.collect(
-                <ThemeProvider theme={theme}>
-                    <Home/>
-                </ThemeProvider>,
-            ),
-        );
-        const css = sheets.toString();
-        response.send(template('Клуб Менторов Физтех-Союза - программа наставничества МФТИ', html, "home", css));
+    pages.map(page => {
+        const PageComponent = require('./src/components/' + page.component_name)[page.component_name];
+        router.get(page.url, (request, response) => {
+            const sheets = new ServerStyleSheets();
+            const html = ReactDOMServer.renderToString(
+                sheets.collect(
+                    <ThemeProvider theme={theme}>
+                        <PageComponent/>
+                    </ThemeProvider>
+                )
+            );
+            const css = sheets.toString();
+            response.send(template(page.title, html, page.script, css));
+        });
     });
 
-    router.get("/News", (request, response) => {
-        const sheets = new ServerStyleSheets();
-        const html = renderToString(
-            sheets.collect(
-                <ThemeProvider theme={theme}>
-                    <News/>
-                </ThemeProvider>,
-            ),
-        );
-        const css = sheets.toString();
-        response.send(template('Новости проекта | Клуб Менторов Физтех-Союза', html, "news", css));
-    });
     router.get("/news_data", (request, response) => {
         client.db('miptmentors').collection("news").find({"publicated": true}).toArray((err, news) => {
             news.sort(function (a, b) {
@@ -51,92 +35,12 @@ export default (passport, client) => {
             response.send(news);
         });
     });
-
-    router.get("/Mentors", (request, response) => {
-        const sheets = new ServerStyleSheets();
-        const html = renderToString(
-            sheets.collect(
-                <ThemeProvider theme={theme}>
-                    <Mentors/>
-                </ThemeProvider>,
-            ),
-        );
-        const css = sheets.toString();
-        response.send(template('Менторы проекта | Клуб Менторов Физтех-Союза', html, "mentors", css));
-    });
     router.get("/mentors_data", (request, response) => {
         client.db('miptmentors').collection("mentors").find().toArray((err, mentors) => {
             response.send(mentors);
         });
     });
 
-    router.get("/Contacts", (request, response) => {
-        const sheets = new ServerStyleSheets();
-        const html = renderToString(
-            sheets.collect(
-                <ThemeProvider theme={theme}>
-                    <Contacts/>
-                </ThemeProvider>,
-            ),
-        );
-        const css = sheets.toString();
-        response.send(template('Контакты | Клуб Менторов Физтех-Союза', html, "contacts", css));
-    });
-
-    router.get("/SignIn", checkIfAuthenticated, (request, response) => {
-        const sheets = new ServerStyleSheets();
-        const html = renderToString(
-            sheets.collect(
-                <ThemeProvider theme={theme}>
-                    <SignIn/>
-                </ThemeProvider>,
-            ),
-        );
-        const css = sheets.toString();
-        response.send(template('Вход | Клуб Менторов Физтех-Союза', html, "signin", css));
-    });
-
-    router.get("/findmentor", (request, response) => {
-        const sheets = new ServerStyleSheets();
-        const html = renderToString(
-            sheets.collect(
-                <ThemeProvider theme={theme}>
-                    <FindMentor/>
-                </ThemeProvider>,
-            ),
-        );
-        const css = sheets.toString();
-        response.send(template('Найти ментора | Клуб Менторов Физтех-Союза', html, "findmentor", css));
-    });
-
-    router.get("/becomementor", (request, response) => {
-        const sheets = new ServerStyleSheets();
-        const html = renderToString(
-            sheets.collect(
-                <ThemeProvider theme={theme}>
-                    <BecomeMentor/>
-                </ThemeProvider>,
-            ),
-        );
-        const css = sheets.toString();
-        response.send(template('Стать ментором | Клуб Менторов Физтех-Союза', html, "becomementor", css));
-    });
-
     return router;
 
-};
-
-/*const checkIfNotAuthenticated = (request, response, next) => {
-    if (request.isAuthenticated()) {
-        next();
-    } else {
-        response.redirect("/SignIn");
-    }
-};*/
-const checkIfAuthenticated = (request, response, next) => {
-    if (request.isAuthenticated()) {
-        response.redirect("/admin");
-    } else {
-        next();
-    }
 };
---
+++ src/components/common/Navbar/Menu/Menu.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import {makeStyles} from '@material-ui/styles';
+import {Grid, List, Box} from '@material-ui/core';
+import {MenuItem} from "./MenuItem";
+import {MenuDrawerOpen} from "./MenuDrawerOpen";
+import {MenuDrawerClose} from "./MenuDrawerClose";
+
+const useStyles = makeStyles(theme => ({
+    menuContainer: {
+        width: 'calc(100% - 179px)',
+        [theme.breakpoints.up('xl')]: {
+            width: 'calc(100% - 207px)'
+        }
+    },
+    menuList: {
+        justifyContent: 'flex-end',
+        padding: '0px'
+    },
+    menuDrawerControls: {
+        position: 'relative',
+        height: '81px',
+        justifyContent: 'flex-end',
+        padding: '0px',
+        overflow: 'hidden'
+    }
+}));
+
+const Menu = props => {
+    const classes = useStyles();
+    return (
+        <Grid item className={classes.menuContainer}>
+            <List component={Box} display={{xs: 'none', md: 'flex'}}
+                  className={classes.menuList}>
+                <MenuItem text='Новости проекта' href='/news' id='news' current={props.current === 'news'}/>
+                <MenuItem text='Менторы проекта' href='/mentors' id='mentors' current={props.current === 'mentors'}/>
+                <MenuItem text='Контакты' href='/contacts' id='contacts' current={props.current === 'contacts'}/>
+                <MenuItem text='Найти ментора' href='/findmentor' id='findmentor' current={props.current === 'findmentor'}/>
+                <MenuItem text='Стать ментором' href='/becomementor' id='becomementor' current={props.current === 'becomementor'}/>
+            </List>
+            <Box display={{xs: 'flex', md: 'none'}}
+                 className={classes.menuDrawerControls}>
+                <MenuDrawerOpen handleDrawerOpenClick={props.handleDrawerOpenClick}
+                                drawerOpen={props.drawerOpen}/>
+                <MenuDrawerClose handleDrawerCloseClick={props.handleDrawerCloseClick}
+                                 drawerOpen={props.drawerOpen}/>
+            </Box>
+        </Grid>
+    );
+};
+
+export {Menu};
