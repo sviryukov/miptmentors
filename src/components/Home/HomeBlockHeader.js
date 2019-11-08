1f7ca7443c9ddbb5116b90c63f215cc4c3085bf0
sviryukov <lirik1198KZ>
2019-11-08 16:34:22 +0300

    Create home block header component

---
+++ src/components/Home/HomeBlockHeader.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import {makeStyles} from "@material-ui/styles";
+import {Grid, Typography} from "@material-ui/core";
+
+const useStyles = makeStyles({
+    homeBlockHeader: {
+        textAlign: 'center',
+        fontSize: '40px',
+        fontWeight: '100'
+    }
+});
+
+const HomeBlockHeader = props => {
+    const classes = useStyles();
+    return (
+        <Grid component={Typography} variant='h2'
+              item xs={12}
+              className={classes.homeBlockHeader}>
+            {props.text}
+        </Grid>
+    );
+};
+
+export {HomeBlockHeader};
