11392a340109b352605467cdea0ea579d89d7c03
sviryukov <lirik1198KZ>
2019-11-22 18:32:02 +0300

    Add light text variant of home block header

--- src/components/Home/HomeBlockHeader.js
+++ src/components/Home/HomeBlockHeader.js
@@ -1,12 +1,23 @@
-import React from 'react'
+import React from 'react';
 import {makeStyles} from "@material-ui/styles";
 import {Grid, Typography} from "@material-ui/core";
 
 const useStyles = makeStyles(theme => ({
     homeBlockHeader: {
+        padding: '100px',
         textAlign: 'center',
         fontSize: '40px',
         fontWeight: '100',
+        [theme.breakpoints.up('md')]: {
+            paddingBottom: '50px'
+        }
+    },
+    homeBlockHeaderLight: {
+        paddingTop: '100px',
+        textAlign: 'center',
+        fontSize: '40px',
+        fontWeight: '100',
+        color: theme.palette.text.light,
         [theme.breakpoints.up('md')]: {
             paddingBottom: '50px'
         }
@@ -18,7 +29,7 @@ const HomeBlockHeader = props => {
     return (
         <Grid component={Typography} variant='h2'
               item xs={12}
-              className={classes.homeBlockHeader}>
+              className={props.light ? classes.homeBlockHeaderLight : classes.homeBlockHeader}>
             {props.text}
         </Grid>
     );
