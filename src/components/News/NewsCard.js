8822931ab3ca68bc74e06106d086f9a8fa660d19
sviryukov <lirik1198KZ>
2020-02-27 17:43:12 +0300

    Change news card render animation

--- src/components/News/NewsCard.js
+++ src/components/News/NewsCard.js
@@ -1,7 +1,6 @@
 import React from "react";
 import {makeStyles} from "@material-ui/styles";
-import {Grid, Card, CardActionArea, CardContent} from "@material-ui/core";
-import {FadeFromBottom} from "../common/animations";
+import {Grid, Fade, Card, CardActionArea, CardContent} from "@material-ui/core";
 import {NewsCardImg} from "./NewsCardImg";
 import {dateFormat} from './dateFormat';
 
@@ -25,20 +24,19 @@ const useStyles = makeStyles(theme => ({
 const NewsCard = props => {
     const classes = useStyles();
     return (
-        <FadeFromBottom on_scroll
-                        component={Grid}
-                        item lg={4} md={6} xs={12}
-                        className={classes.newsCard}>
-            <Card>
-                <CardActionArea>
-                    <NewsCardImg image={props.img.replace('.', 'min.')} tag={props.tag} alt={props.title}/>
-                    <CardContent className={classes.newsCardContent}>
-                        <p className={classes.newsCardTitle}>{props.title}</p>
-                        <p className={classes.newsCardDate}>{dateFormat(props.date)}</p>
-                    </CardContent>
-                </CardActionArea>
-            </Card>
-        </FadeFromBottom>
+        <Grid item lg={4} md={6} xs={12}>
+            <Fade in={props.visible} timeout={500}>
+                <Card>
+                    <CardActionArea>
+                        <NewsCardImg image={props.img.replace('.', 'min.')} tag={props.tag} alt={props.title}/>
+                        <CardContent className={classes.newsCardContent}>
+                            <p className={classes.newsCardTitle}>{props.title}</p>
+                            <p className={classes.newsCardDate}>{dateFormat(props.date)}</p>
+                        </CardContent>
+                    </CardActionArea>
+                </Card>
+            </Fade>
+        </Grid>
     );
 };
 
