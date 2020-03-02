26ad14642f8c4f4488cc93daf228e927c6d40a0d
sviryukov <lirik1198KZ>
2020-03-02 17:13:56 +0300

    Add news item page related requests to route.js

--- route.js
+++ route.js
@@ -5,6 +5,8 @@ import {ServerStyleSheets, ThemeProvider} from "@material-ui/styles";
 import template from './src/template';
 import theme from './src/theme';
 import pages from './src/pages';
+import { ObjectID } from 'mongodb';
+import { NewsItem } from './src/components/NewsItem'
 
 export default (passport, client) => {
 
@@ -26,6 +28,30 @@ export default (passport, client) => {
         });
     });
 
+    router.get("/news/:id", (request, response) => {
+        try {
+            let _id = ObjectID(request.params.id);
+            client.db('miptmentors').collection("news").findOne({"_id": _id}).then(newsItem => {
+                if (newsItem.publicated === 'false') {
+                    response.redirect("/news");
+                } else {
+                    const sheets = new ServerStyleSheets();
+                    const html = ReactDOMServer.renderToString(
+                        sheets.collect(
+                            <ThemeProvider theme={theme}>
+                                <NewsItem/>
+                            </ThemeProvider>
+                        )
+                    );
+                    const css = sheets.toString();
+                    response.send(template(newsItem.title, html, 'newsitem', css));
+                }
+            });
+        } catch(err) {
+            response.redirect("/news");
+        }
+    });
+
     router.get("/news_data", (request, response) => {
         client.db('miptmentors').collection("news").find({"publicated": "true"}).toArray((err, news) => {
             news.sort(function (a, b) {
@@ -34,6 +60,20 @@ export default (passport, client) => {
             if (request.query.limited) news = news.slice(0, 2);
             response.send(news);
         });
+    });
+    router.get("/news_item_data", (request, response) => {
+        try {
+            let _id = ObjectID(request.query._id);
+            client.db('miptmentors').collection("news").findOne({_id: _id}).then(newsItem => {
+                if (newsItem.publicated === "true") {
+                    response.send(newsItem);
+                } else {
+                    response.sendStatus('400');
+                }
+            });
+        } catch (e) {
+            response.sendStatus('400');
+        }
     });
     router.get("/mentors_data", (request, response) => {
         let query;
