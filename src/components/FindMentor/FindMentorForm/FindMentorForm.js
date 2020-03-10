550f00294cab1d9223e44824681dba83e8211918
sviryukov <lirik1198KZ>
2020-03-10 15:04:21 +0300

    Disable find mentor form

--- src/components/FindMentor/FindMentorForm/FindMentorForm.js
+++ src/components/FindMentor/FindMentorForm/FindMentorForm.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {makeStyles} from '@material-ui/styles';
 import {Grid, Box} from '@material-ui/core';
 import {FormHeader} from "../../common/forms/FormHeader";
-import {ShortTextField} from "../../common/forms/ShortTextField";
+/*import {ShortTextField} from "../../common/forms/ShortTextField";
 import {FindMentorFormDepartment} from "./FindMentorFormDepartment";
 import {FindMentorFormYear} from "./FindMentorFormYear";
 import {Textarea} from "../../common/forms/Textarea";
@@ -11,12 +11,13 @@ import {LongTextField} from "../../common/forms/LongTextField";
 import {FindMentorFormAccelerators} from "./FindMentorFormAccelerators";
 import {PublicationConsent} from "../../common/forms/PublicationConsent";
 import {ProcessingConsent} from "../../common/forms/ProcessingConsent";
-import {SendButton} from "../../common/forms/SendButton";
+import {SendButton} from "../../common/forms/SendButton";*/
 
 const useStyles = makeStyles(theme => ({
     findMentorForm: {
         borderRadius: '4px',
         padding: '80px 50px 40px',
+        fontSize: '19px',
         [theme.breakpoints.up('sm')]: {
             padding: '100px 50px 40px'
         },
@@ -39,7 +40,12 @@ const FindMentorForm = () => {
               item xs={12} md={7} lg={6} xl={5}
               className={classes.findMentorForm}>
             <FormHeader text='Найти ментора'/>
-            <ShortTextField label='Имя *' name='name'/>
+            <br/>
+            Регистрация на 5-й сезон Клуба Менторов Фи.зтех-Союза закрыта.
+            <br/>
+            <br/>
+            Присоединяйтесь к нам в следующем сезоне в октябре 2020 года.
+            {/*<ShortTextField label='Имя *' name='name'/>
             <ShortTextField label='Фамилия *' name='surname'/>
             <FindMentorFormDepartment/>
             <FindMentorFormYear/>
@@ -53,7 +59,7 @@ const FindMentorForm = () => {
             <FindMentorFormAccelerators/>
             <PublicationConsent/>
             <ProcessingConsent/>
-            <SendButton/>
+            <SendButton/>*/}
         </Grid>
     );
 };
