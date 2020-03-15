5419f21fcfd2e5e350766aaff78891510ba615aa
sviryukov <lirik1198KZ>
2020-03-16 00:32:12 +0300

    Fix home mentor card component name and class names

--- src/components/Home/HomeMentors/HomeMentorCard.js
+++ src/components/Home/HomeMentors/HomeMentorCard.js
@@ -3,23 +3,25 @@ import {makeStyles} from "@material-ui/styles";
 import {Card, CardContent, CardMedia, Grid} from "@material-ui/core";
 
 const useStyles = makeStyles({
-    homeMentor: {
+    homeMentorCard: {
         margin: '16px'
     },
-    homeMentorCardName: {
+    homeMentorCardHeader: {
         fontWeight: 'bold'
     }
 });
 
-const HomeMentor = props => {
+const HomeMentorCard = props => {
     const classes = useStyles();
     return (
         <Grid component={Card}
               item xs={10} sm={3} lg={2}
-              className={classes.homeMentor}>
+              className={classes.homeMentorCard}>
             <CardMedia image={'/img/' + props.img} component="img"/>
-            <CardContent className={classes.homeMentorCardContent}>
-                <p className={classes.homeMentorCardName}>{props.name}</p>
+            <CardContent>
+                <p className={classes.homeMentorCardHeader}>
+                    {props.name}
+                </p>
                 {props.education}
                 <br/>
                 {props.work}
@@ -28,4 +30,4 @@ const HomeMentor = props => {
     );
 };
 
-export {HomeMentor};
+export {HomeMentorCard};
--- src/components/Home/HomeMentors/HomeMentors.js
+++ src/components/Home/HomeMentors/HomeMentors.js
@@ -3,12 +3,12 @@ import {makeStyles} from "@material-ui/styles";
 import axios from "axios";
 import {Grid} from "@material-ui/core";
 import {HomeBlockHeader} from "../HomeBlockHeader";
-import {HomeMentor} from "./HomeMentor";
+import {HomeMentorCard} from "./HomeMentorCard";
 import {HomeBlockLink} from "../HomeBlockLink";
 
 const useStyles = makeStyles(theme => ({
     homeMentors: {
-        padding : '75px 0px',
+        padding: '75px 0px',
         borderBottom: '1px solid ' + theme.palette.divider,
         justifyContent: 'center',
         [theme.breakpoints.up('md')]: {
@@ -36,11 +36,11 @@ const HomeMentors = () => {
               className={classes.homeMentors}>
             <HomeBlockHeader text={'Наши менторы'}/>
             {mentors.map(mentor => (
-                <HomeMentor key={mentor.name}
-                            name={mentor.name}
-                            img={mentor.img}
-                            education={mentor.education}
-                            work={mentor.work}/>
+                <HomeMentorCard key={mentor.name}
+                                name={mentor.name}
+                                img={mentor.img}
+                                education={mentor.education}
+                                work={mentor.work}/>
             ))}
             <HomeBlockLink href={'/mentors'} text={'Все менторы'}/>
         </Grid>
