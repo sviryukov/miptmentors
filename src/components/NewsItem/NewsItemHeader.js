92f664623f99ba6e42bed7574419132738cd11e4
sviryukov <lirik1198KZ>
2020-03-16 00:45:08 +0300

    Remove unnecessary tag and class from hews item header component

--- src/components/NewsItem/NewsItemHeader.js
+++ src/components/NewsItem/NewsItemHeader.js
@@ -5,17 +5,15 @@ import {Grid, Typography} from "@material-ui/core";
 const useStyles = makeStyles(theme => ({
     newsItemHeaderContainer: {
         padding: "70px 0px 20px",
+        fontSize: '30px',
+        textAlign: 'left',
+        color: theme.palette.text.primary,
         [theme.breakpoints.up('md')]: {
             padding: "80px 0px 20px",
         },
         [theme.breakpoints.up('lg')]: {
             padding: "100px 0px 20px",
         }
-    },
-    newsItemHeader: {
-        fontSize: '30px',
-        textAlign: 'left',
-        color: theme.palette.text.primary
     }
 }));
 
@@ -23,10 +21,9 @@ const NewsItemHeader = props => {
     const classes = useStyles();
     return (
         <Grid item xs={12}
+              component={Typography} variant='h1'
               className={classes.newsItemHeaderContainer}>
-            <Typography variant='h1' className={classes.newsItemHeader}>
-                {props.text}
-            </Typography>
+            {props.text}
         </Grid>
     );
 };
