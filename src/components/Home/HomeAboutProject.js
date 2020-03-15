12a88a7daaae788b6c2fa1e3ea2574d9df362f90
sviryukov <lirik1198KZ>
2020-03-16 00:21:54 +0300

    Fix class name, remove unnecessary class, styles and tags

--- src/components/Home/HomeAboutProject.js
+++ src/components/Home/HomeAboutProject.js
@@ -3,37 +3,31 @@ import {makeStyles} from "@material-ui/styles";
 import {Grid, Typography} from "@material-ui/core";
 
 const useStyles = makeStyles(theme => ({
-    homeAboutProjectContainer: {
+    homeAboutProject: {
         padding: '75px 0px',
         borderBottom: '1px solid ' + theme.palette.divider,
         backgroundColor: theme.palette.background.paper,
-        justifyContent: 'center',
         textAlign: 'center',
+        fontWeight: '100',
+        fontSize: '19px',
+        [theme.breakpoints.up('sm')]: {
+            fontSize: '22px'
+        },
         [theme.breakpoints.up('md')]: {
-            padding: '100px 0px'
+            padding: '100px 0px',
+            fontSize: '24px'
         }
     },
     homeAboutProjectImg: {
         width: '250px',
         marginBottom: '20px'
     },
-    homeAboutProjectName: {
-        fontWeight: '100',
+    homeAboutProjectHeader: {
+        marginBottom: '20px',
         color: theme.palette.secondary.main,
         fontSize: '30px',
         [theme.breakpoints.up("md")]: {
             fontSize: '40px'
-        }
-    },
-    homeAboutProjectText: {
-        fontWeight: '100',
-        paddingTop: '20px',
-        fontSize: '19px',
-        [theme.breakpoints.up('200pxsm')]: {
-            fontSize: '22px'
-        },
-        [theme.breakpoints.up('md')]: {
-            fontSize: '24px'
         }
     }
 }));
@@ -41,19 +35,19 @@ const useStyles = makeStyles(theme => ({
 const HomeAboutProject = () => {
     const classes = useStyles();
     return (
-        <Grid container item xs={12} className={classes.homeAboutProjectContainer} id={'learn-more-target'}>
+        <Grid container justify='center'
+              item xs={12}
+              className={classes.homeAboutProject} id={'learn-more-target'}>
             <Grid item xs={11} sm={9} md={8} lg={6}>
                 <img className={classes.homeAboutProjectImg} src={'/img/logo.svg'} alt={'Клуб Менторов Физтех-Союза'}/>
-                <Typography variant='h2' className={classes.homeAboutProjectName}>
+                <Typography variant='h2' className={classes.homeAboutProjectHeader}>
                     Клуб Менторов Физтех&#8209;Союза
                 </Typography>
-                <Typography className={classes.homeAboutProjectText}>
-                    Проект, который помогает студентам и молодым выпускникам МФТИ найти наставника среди опытных специалистов и
-                    предпринимателей.
-                </Typography>
-                <Typography className={classes.homeAboutProjectText}>
-                    В проекте реализуются программы наставничества по трем направлениям: бизнес, карьерное и научное.
-                </Typography>
+                Проект, который помогает студентам и молодым выпускникам МФТИ найти наставника среди опытных специалистов и
+                предпринимателей.
+                <br/>
+                <br/>
+                В проекте реализуются программы наставничества по трем направлениям: бизнес, карьерное и научное.
             </Grid>
         </Grid>
     );
