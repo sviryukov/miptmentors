0542352ecd2d602ddd5ed7557d50c6f5454cf3f2
sviryukov <lirik1198KZ>
2020-03-16 00:55:12 +0300

    Remove unnecessary tag and class from page header component

--- src/components/common/PageHeader.js
+++ src/components/common/PageHeader.js
@@ -3,18 +3,16 @@ import {makeStyles} from "@material-ui/styles";
 import {Grid, Typography} from "@material-ui/core";
 
 const useStyles = makeStyles(theme => ({
-    pageHeaderContainer: {
+    pageHeader: {
         padding: "70px 0px 40px",
+        textAlign: 'center',
+        color: theme.palette.text.primary,
         [theme.breakpoints.up('md')]: {
             padding: "80px 0px 40px",
         },
         [theme.breakpoints.up('lg')]: {
             padding: "100px 0px 40px",
         }
-    },
-    pageHeader: {
-        textAlign: 'center',
-        color: theme.palette.text.primary
     }
 }));
 
@@ -22,10 +20,9 @@ const PageHeader = props => {
     const classes = useStyles();
     return (
         <Grid item xs={12}
-              className={classes.pageHeaderContainer}>
-            <Typography variant='h1' className={classes.pageHeader}>
-                {props.text}
-            </Typography>
+              component={Typography} variant='h1'
+              className={classes.pageHeader}>
+            {props.text}
         </Grid>
     );
 };
