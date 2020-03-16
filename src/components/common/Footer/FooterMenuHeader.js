f0a34cd208066cf25011ef5fe408ae0c6da28812
sviryukov <lirik1198KZ>
2020-03-16 12:57:35 +0300

    Change footer menu header tag to div

--- src/components/common/Footer/FooterMenuHeader.js
+++ src/components/common/Footer/FooterMenuHeader.js
@@ -1,10 +1,8 @@
 import React from "react";
 import {makeStyles} from "@material-ui/styles";
-import {Box} from "@material-ui/core";
 
 const useStyles = makeStyles(theme => ({
     footerMenuHeader: {
-        display: 'block',
         height: '104px',
         padding: '30px 0px',
         fontSize: '16px',
@@ -23,9 +21,9 @@ const useStyles = makeStyles(theme => ({
 const FooterMenuHeader = props => {
     const classes = useStyles();
     return (
-        <Box className={classes.footerMenuHeader}>
+        <div className={classes.footerMenuHeader}>
             {props.text}
-        </Box>
+        </div>
     );
 };
 
