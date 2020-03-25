e33588338ece7cdadf79c38c3b3d8075c5f52f19
sviryukov <lirik1198KZ>
2020-03-25 22:34:41 +0300

    Add light variant of page header

--- src/components/common/PageHeader.js
+++ src/components/common/PageHeader.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import {makeStyles} from "@material-ui/styles";
+import theme from '../../theme';
 import {Grid, Typography} from "@material-ui/core";
 
 const useStyles = makeStyles(theme => ({
     pageHeader: {
         padding: "70px 0px 40px",
         textAlign: 'center',
-        color: theme.palette.text.primary,
         [theme.breakpoints.up('md')]: {
             padding: "80px 0px 40px",
         },
@@ -21,7 +21,8 @@ const PageHeader = props => {
     return (
         <Grid item xs={12}
               component={Typography} variant='h1'
-              className={classes.pageHeader}>
+              className={classes.pageHeader}
+              style={{ color: props.color === 'light' ? theme.palette.text.light : theme.palette.text.primary}}>
             {props.text}
         </Grid>
     );
